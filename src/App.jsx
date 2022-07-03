8833c63353029dcc6b9806fe6a13308240900b25
ShoheiTakino <104742356+ShoheiTakino@users.noreply.github.com>
2022-07-03 16:00:49 +0900

    useEffectについて

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,41 @@
-import React, { useState } from "react";
+/*eslint react-hooks/exhaustive-deps: off*/
+
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("最初");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]); //numに反応してnumに関係ある挙動の時のみuseEffect内の分離ができる。
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfulMessage color="blue">お元気ですか?</ColorfulMessage>
       <ColorfulMessage color="pink">お元気で</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>^_^</p>}
     </>
   );
 };
